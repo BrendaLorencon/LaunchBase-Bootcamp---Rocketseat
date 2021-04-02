71a30800aa8ccbe2944e33656e75cdc4afe6da75
Brenda Lorençon <brenda.silva.avatar@gmail.com>
2021-04-01 22:45:38 -0300

    Iniciando Java Script e seus opeadores!

---
+++ index.js
@@ -0,0 +1,28 @@
+// Criar um programa que calcula a média
+// das notas entre alunos e envia
+// mensagem do cálculo da média.
+
+const aluno01 = 'Brenda'
+const notaAluno01 = 9.8
+
+const aluno02 = 'Mayke'
+const notaAluno02 = 10
+
+const aluno03 = 'Eder'
+const notaAluno03 = 2
+
+
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+// Condicionais - Se a média for maior que 5, parabenizar a turma
+
+if (media > 5) {
+    console.log(`A média foi de ${media}. Parabéns.`)
+} else {
+    console.log('A média é menor que 5.')
+}
+
+
+
+
---
+++ operadores.js
@@ -0,0 +1,68 @@
+// Operadores Relacionais
+
+// >   Maior que
+// <   Menor que
+// >=  Maior igual a
+// <=  Menor igual a
+
+// const a = 5
+// const b = 6
+
+// console.log( a > b ) // false
+// console.log( a < b ) // true
+// console.log( a >= b ) // false
+// console.log( a <= b ) // true
+
+
+// Operadores de Igualdade
+
+// == Igual a
+// != Diferente de
+// === Igual e do mesmo tipo
+// !== Diferente inclusive do tipo
+
+// const a = 5
+// const b = '5'
+
+// console.log( a == b ) // true
+// console.log( a === b )// false
+// console.log( a != b ) // false
+// console.log( a !== b )// true
+
+// Operadores Lógicos
+
+// São tipocamente usados com valores booleanos (true or false)
+// Nos ajudam a criar expressões lógicas a partir de dois ou mais valores
+
+// && "E"  Vai comparar dois booleanos, e retornar verdadeiro caso AS DUAS
+//         expressões sejam verdadeiras.
+// || "OU" Vai comparar dois booleanos, e retornar verdadeiro caso UMA DAS DUAS
+//         expressões sejam verdadeiras.
+// ! "NAO" Vai negar uma expressão! 
+
+// const a = 5
+// const b = '5'
+
+// console.log( a == b && a !== b ) // true
+// console.log( a == b && a != b ) // false
+
+// console.log( a == b || a !== b ) // true
+// console.log( a == b || a != b ) //  true
+// console.log( a != b || a === b ) // false
+
+// console.log( !(a == b || a != b) ) // false
+// console.log( !true ) // false
+
+// Operadores Aritméticos
+
+// *    Multiplicação
+// /    Divisão
+// %    Resto da Divisão
+// +    Adição
+// -    Subtração
+
+console.log( 2 * 2 ) // 4
+console.log( 2 / 2 ) // 1
+console.log( 2 % 1.4 ) 
+console.log( 2 + 2 ) // 4
+console.log( 2 - 2) // 0
