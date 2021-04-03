894ba94c03b7901d11f709519eef343cbab5de21
Brenda Lorençon <brenda.silva.avatar@gmail.com>
2021-04-03 16:48:15 -0300

    Entendendo métodos e funções.

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 // Criar um programa que calcula a média
-// das notas entre alunos e envia
+// das turmas de alunos e envia 
 // mensagem do cálculo da média.
 
-const aluno01 = 'Brenda'
-const notaAluno01 = 9.8
 
-const aluno02 = 'Mayke'
-const notaAluno02 = 10
+const alunosDaTurmaA = [
+    {
+        nome: "Mayk",
+        nota: 1.8
+    },
+    {
+        nome: "Brenda",
+        nota: 5
+    },
+    {   
+        nome: "Eder",
+        nota: 3
+    }
+]
 
-const aluno03 = 'Eder'
-const notaAluno03 = 2
+const alunosDaTurmaB = [
+    {
+        nome: "Joao",
+        nota: 5
+    },
+    {
+        nome: "Leonardo",
+        nota: 10
+    },
+    {   
+        nome: "Felipe",
+        nota: 10
+    }
+]
 
-
-
-const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
-
-// Condicionais - Se a média for maior que 5, parabenizar a turma
-
-if (media > 5) {
-    console.log(`A média foi de ${media}. Parabéns.`)
-} else {
-    console.log('A média é menor que 5.')
+function calculaMedia(alunos){
+    return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 }
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma){
+    // Se a média for maior que 5, parabenizar a turma
+    if (media > 5) {
+        console.log(`A média da  ${turma} foi de ${media}. Parabéns.`)
+    } else {
+        console.log(`A média da  ${turma} é menor que 5.`)
+    }
+
+}
+
+enviaMensagem(media1, 'turmaA')
+enviaMensagem(media2, 'turmaB')
 
 
 
