e453fc3683377f9767afc58ea8deb8c179f5a379
BrendaLorencon <brenda.silva.avatar@gmail.com>
2021-04-18 23:17:51 -0300

    Tornando os cards Dinâmicos com Iframe.

--- scripts.js
+++ scripts.js
@@ -3,10 +3,15 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function(){
+        const paginaId = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src =`https://${paginaId}`
+
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
 })
+
